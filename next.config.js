502b4b771f62fc8150c81f2c4512593668f1a582
Yobel <vega.yobel.2@gmail.com>
2023-09-20 18:45:50 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,29 +1,31 @@
+const { env } = require("process");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
- 
+  output: "export",
+
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
- 
+
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   // skipTrailingSlashRedirect: true,
- 
+
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
-  
+
   // Add basePath
-  basePath: '/Restourant',
+  basePath: env.production && "/Restourant",
 
   images: {
-   remotePatterns: [
+    remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'yobel47.github.io',
-        port: '',
-        pathname: '/Restourant/**',
+        protocol: "https",
+        hostname: "yobel47.github.io",
+        port: "",
+        pathname: "/Restourant/**",
       },
     ],
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
